6b8366229a1313ea1a79e1327460e60dddde823f
nmvuong12 <vuong179301@gmail.com>
2024-12-23 01:23:27 +0700

    Cập nhật lại mối quan hệ

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -9,6 +9,8 @@ class Customer extends Model
     //
     public $timestamps = false; // Tắt timestamps
 
+    protected $fillable = ['name', 'address', 'phone', 'email'];
+    
     public function orders()
     {
         return $this->hasMany(Order::class);
