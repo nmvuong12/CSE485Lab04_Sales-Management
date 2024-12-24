172c1ca7881e7802c10fc6c1e2bf007a8c8b4ca7
Hoang <viethoang101004@gmail.com>
2024-12-24 14:41:30 +0700

    cap nhat order_details table

--- database/migrations/2024_12_22_074528_create_order_details_table.php
+++ database/migrations/2024_12_22_074528_create_order_details_table.php
@@ -13,13 +13,11 @@ return new class extends Migration
     {
         Schema::create('order_details', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('order_id');
-            $table->unsignedBigInteger('product_id');
+            $table->foreignId('order_id')->constrained()->onDelete('cascade');
+            $table->foreignId('product_id')->constrained()->onDelete('cascade');
             $table->integer('quantity');
             $table->timestamps();
-
-            $table->foreign('order_id')->references('id')->on('orders');
-            $table->foreign('product_id')->references('id')->on('products');
+            
         });
     }
 
